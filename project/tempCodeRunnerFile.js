b2339a086b7ff61818867d02030bb9bba6616729
Hasmik-icml <gevorgyan.icml@gmail.com>
2020-12-03 16:46:48 +0400

    2-th commit

--- project/tempCodeRunnerFile.js
+++ project/tempCodeRunnerFile.js
@@ -1,8 +1,12 @@
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+let y = 10;
+let div = function(t, s){
+  return t/s;
+};
+let dip=function(y, zoo){
+  return zoo(y)*zoo(y);
+};
+let yup=function(c){
+  return div(y,2);
+};
+let d = dip(4, yup);
+console.log(d);
